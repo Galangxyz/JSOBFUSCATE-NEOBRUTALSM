65b08f61026c23c1c412360e56ac172196c531bc
M Galang febriansyah pratama <wibukntl858@gmail.com>
2025-05-05 14:26:09 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'John Doe',
+  title: 'Langz Panel Js-Obfuscate',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-96x96.png',
